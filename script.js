a1e86b1a4d7f91e805b0514b78ff22a91ae518c8
Tejaswini Khairnar <117677072+Tejaswini1397@users.noreply.github.com>
2023-11-14 09:37:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 var dateDiffInDays = function (date1, date2) {
   //   write your code here
+ const timeDiff = Math.abs(date2.getTime() - date1.getTime());
+
+  // Calculate the number of days
+  const daysDiff = Math.ceil(timeDiff / (1000 * 3600 * 24));
+
+  return daysDiff;
+	
+	
 };
 
 // Do not change the code below.
 const dateOne = prompt("Enter Start Date.");
 const dateTwo = prompt("Enter End Date.");
-alert(dateDiffInDays(dateOne, dateTwo));
+// alert(dateDiffInDays(dateOne, dateTwo));
